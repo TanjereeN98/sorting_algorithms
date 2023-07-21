3542ea9b5db2c3358de5c1fc5e72b72f7b1471c8
TanjereeN98 <ahmadcyc35@gmail.com>
2023-07-22 02:50:16 +0300

    Adding the first task {0. Bubble sort}

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+#include <stdbool.h>
+/**
+ * bubble_sort - sorts an array of integers in ascending
+ * order using the Bubble sort algorithm
+ *
+ * @array: the array to sort
+ * @size: size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t j;
+	bool sorted = false;
+	size_t temp_size = size;
+
+	while (!sorted)
+	{
+		sorted = true;
+		for (j = 0 ; j < temp_size - 1 ; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				int temp = array[j];
+
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+				sorted = false;
+			}
+		}
+		temp_size--;
+	}
+}
