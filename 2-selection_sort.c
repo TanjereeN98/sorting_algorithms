9ae7bb5bae62f27c6cd6067dcde306fd946d4518
TanjereeN98 <ahmadcyc35@gmail.com>
2023-07-22 16:19:21 +0300

    Fixing The selection algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,7 +15,7 @@ void selection_sort(int *array, size_t size)
 
 	if (!array)
 		return;
-	for (i = 0 ; i < size - 2 ; i++)
+	for (i = 0 ; i < size - 1 ; i++)
 	{
 		iMin = i;
 		for (j = i + 1 ; j < size ; j++)
