9128a9655279b0ff724444f6f5ac31960b4b3e41
TanjereeN98 <ahmadcyc35@gmail.com>
2023-07-22 16:35:52 +0300

    Adding a flag to indicate no swaps were done

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,21 +11,26 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t iMin, j, i = 0;
-	int temp;
+	int temp, flag = 0;
 
 	if (!array || size < 1)
 		return;
-	for (i = 0 ; i < size - 1 ; i++)
+	for (i = 0 ; i < size ; i++)
 	{
 		iMin = i;
-		for (j = i + 1 ; j < size ; j++)
+		for (j = i + 1 ; j < size; j++)
 		{
 			if (array[iMin] > array[j])
+			{
 				iMin = j;
+				flag = 1;
+			}
 		}
 		temp = array[i];
 		array[i] = array[iMin];
 		array[iMin] = temp;
-		print_array(array, size);
+		if (flag)
+			print_array(array, size);
+		flag = 0;
 	}
 }
